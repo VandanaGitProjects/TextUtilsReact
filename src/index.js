1ce75a930bac1df0258f68611659594426247088
Vandana Bhavsar <vrab2010@gmail.com>
2024-11-30 14:31:03 +0530

    forth commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from "react-router-dom";
 // import { BrowserRouter } from "react-router-dom/BrowserRouter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <React.StrictMode>
-    <App />
-   </React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
   // </BrowserRouter>
 );
 
